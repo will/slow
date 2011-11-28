89bb8b28a9ea0cf5b02dd6e3bcfe0d0f752cb70b
Will Leinweber <will@bitfission.com>
2011-11-27 23:39:57 -0800

    kilter

--- clock.rb
+++ clock.rb
@@ -3,4 +3,8 @@ include Clockwork
 handler do |job|
   `curl http://slow.herokuapp.com/`
 end
-every(1.minute, 'ping')
+
+every(42.seconds, 'ping')
+every( 2.minutes, 'ping')
+every( 7.minutes, 'ping')
+every(13.minutes, 'ping')
